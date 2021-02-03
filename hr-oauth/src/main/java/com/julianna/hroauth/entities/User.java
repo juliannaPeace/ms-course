12896438e84ebb1bb3eace746aac99c2d2f09b08
Julianna Paz <jupazlinhares86@gmail.com>
2021-02-03 12:18:06 -0300

    Login, Jwt Token

--- hr-oauth/src/main/java/com/julianna/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/julianna/hroauth/entities/User.java
@@ -1,16 +1,22 @@
 package com.julianna.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
-	
+
 	private String email;
 	private String password;
 
@@ -59,7 +65,7 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
@@ -86,6 +92,37 @@ public class User implements Serializable {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
 		return true;
 	}
 
--- hr-oauth/src/main/java/com/julianna/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/julianna/hroauth/services/UserService.java
@@ -1,13 +1,16 @@
 package com.julianna.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.julianna.hroauth.entities.User;
 import com.julianna.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -17,6 +20,17 @@ public class UserService {
 
 		if (user == null) {
 			throw new IllegalArgumentException("Email not found");
+		}
+
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			throw new UsernameNotFoundException("Email not found");
 		}
 
 		return user;
